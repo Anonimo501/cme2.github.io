f74e912be0f2d1fdf3453a14bdd45dea8c23607a
Anonimo501 <67207446+Anonimo501@users.noreply.github.com>
2024-08-15 09:11:36 -0500

    Add files via upload

---
+++ Introduction - CrackMapExec ~ CME WIKI_files/polyfill.js
@@ -0,0 +1,44 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+/* Polyfill service v3.25.1
+ * For detailed credits and licence information see https://github.com/financial-times/polyfill-service.
+ * 
+ * Features requested: Intl.~locale.en
+ *  */
+
+
+/* No polyfills needed for current settings and browser */
+
+
+
+}
+/*
+     FILE ARCHIVED ON 11:40:21 Mar 20, 2023 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 02:18:50 Aug 15, 2024.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 0.542
+  exclusion.robots: 0.016
+  exclusion.robots.policy: 0.007
+  esindex: 0.01
+  cdx.remote: 8.397
+  LoadShardBlock: 174.534 (6)
+  PetaboxLoader3.datanode: 145.475 (7)
+  PetaboxLoader3.resolve: 109.872 (2)
+  load_resource: 151.033
+*/
